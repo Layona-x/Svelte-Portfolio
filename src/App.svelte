3f0edc755d519bd24d707eeffb85ba277d80d2f0
Layona-x <109739779+Layona-x@users.noreply.github.com>
2022-12-03 15:01:07 +0300

    Add files via upload

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,15 @@
+<script>
+import Navbar from "./components/Navbar.svelte"
+import Header from "./components/Header.svelte"
+import About from "./components/About.svelte"
+import Developer from "./components/Developer-Skill.svelte"
+import Contact from "./components/Contact.svelte"
+</script>
+
+<!-- Navbar here -->
+<Navbar />
+<Header />
+<About />
+<Developer />
+<Contact />
 
